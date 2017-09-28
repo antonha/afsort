a2829b562355098dddeadd2548ef1e5e45225293
Anton Hägerstrand <anton.hagerstrand@meltwater.com>
2017-09-28 19:02:43 +0000

    Reworked to allow sort_by.

--- src/lib.rs
+++ src/lib.rs
@@ -8,34 +8,51 @@ extern crate quickcheck;
 
 pub mod afsort {
 
-    pub fn sort(vec: &mut [String]) {
-        sort_req(vec, 0);
+    pub fn sort<'a, T>(vec: &'a mut [T])
+    where
+        T: AsRef<[u8]>,
+    {
+        sort_by(vec, (|s| s.as_ref()));
     }
 
-    fn sort_req(vec: &mut [String], depth: usize) {
+    pub fn sort_by<T, F>(vec: &mut [T], to_slice: F)
+    where
+        F: Fn(&T) -> &[u8],
+    {
+        sort_req(vec, &to_slice, 0);
+    }
+
+    fn sort_req<T, F>(vec: &mut [T], to_slice: &F, depth: usize)
+    where
+        F: Fn(&T) -> &[u8],
+    {
         if vec.len() <= 32 {
-            vec.sort_unstable();
+            vec.sort_unstable_by(|e1, e2| to_slice(e1).cmp(to_slice(e2)));
             return;
         }
         let mut min = u16::max_value();
         let mut max = 0u16;
         for elem in vec.iter() {
-            if elem.len() > depth {
-                let val = elem.as_bytes()[depth] as u16;
-                if val < min {
-                    min = val;
+            let val = to_slice(elem);
+            if val.len() > depth {
+                let radix_val = val[depth] as u16;
+                if radix_val < min {
+                    min = radix_val;
                 }
-                if val > max {
-                    max = val;
+                if radix_val > max {
+                    max = radix_val;
                 }
             }
         }
+        if min == u16::max_value() {
+            return;
+        }
 
-        let num_items = (max - min + 2) as u16;
+        let num_items = (max - min + 2) as usize;
         let mut counts: Vec<usize> = vec![0usize; num_items as usize];
 
         for elem in vec.iter() {
-            let radix_val = radix_for_str(elem, depth, min);
+            let radix_val = radix_for_str(to_slice(elem), depth, min);
             counts[radix_val as usize] += 1;
         }
 
@@ -47,14 +64,14 @@ pub mod afsort {
         }
         let mut next_free = offsets.clone();
 
-        let mut block = 0u16;
+        let mut block = 0usize;
         let mut i = 0usize;
         while block < num_items - 1 {
             if i >= offsets[block as usize + 1] as usize {
                 block += 1;
             } else {
-                let radix_val = radix_for_str(&vec[i as usize], depth, min);
-                if radix_val == block {
+                let radix_val = radix_for_str(to_slice(&vec[i]), depth, min);
+                if radix_val == block as u16 {
                     i += 1;
                 } else {
                     vec.swap(i as usize, next_free[radix_val as usize] as usize);
@@ -62,18 +79,19 @@ pub mod afsort {
                 }
             }
         }
-        for i in 1..num_items - 1 {
+        for i in 0..num_items - 1 {
             sort_req(
                 &mut vec[offsets[i as usize] as usize..offsets[i as usize + 1] as usize],
+                to_slice,
                 depth + 1,
             );
         }
-        sort_req(&mut vec[offsets[num_items as usize - 1]..], depth + 1);
+        sort_req(&mut vec[offsets[num_items - 1]..], to_slice, depth + 1);
     }
 
-    fn radix_for_str(s: &str, d: usize, base: u16) -> u16 {
-        if s.as_bytes().len() > d {
-            (s.as_bytes()[d] as u16 + 1 - base)
+    fn radix_for_str(s: &[u8], d: usize, base: u16) -> u16 {
+        if s.len() > d {
+            s[d] as u16 + 1 - base
         } else {
             0
         }
@@ -91,19 +109,35 @@ mod tests {
     use rand::{self, Rng};
 
     #[test]
-    fn sorts_same_as_unstable() {
-        QuickCheck::new().tests(100000).quickcheck(
+    fn sorts_strings_same_as_unstable() {
+        fn compare_sort(mut strings: Vec<String>) -> bool {
+            let mut copy = strings.clone();
+            copy.sort_unstable();
+            afsort::sort(&mut strings);
+            strings == copy
+        }
+        QuickCheck::new().tests(1000).quickcheck(
             compare_sort as
                 fn(Vec<String>) -> bool,
         );
     }
 
-    fn compare_sort(mut strings: Vec<String>) -> bool {
-        let mut copy = strings.clone();
-        copy.sort_unstable();
-        afsort::sort(&mut strings);
-        strings == copy
+    #[test]
+    fn sorts_tuples_same_as_unstable() {
+        fn compare_sort(mut tuples: Vec<(String, u8)>) -> bool {
+            let mut copy = tuples.clone();
+            copy.sort_unstable_by(|t1, t2| t1.0.cmp(&t2.0));
+            afsort::sort_by(&mut tuples, |t| t.0.as_bytes());
+            tuples.into_iter().map(|t| t.0).collect::<Vec<String>>() ==
+                copy.into_iter().map(|t| t.0).collect::<Vec<String>>()
+        }
+        QuickCheck::new().tests(1000).quickcheck(
+            compare_sort as
+                fn(Vec<(String, u8)>) -> bool,
+        );
     }
+
+
 
     #[bench]
     fn std_sort(b: &mut Bencher) {
